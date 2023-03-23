7a9bf952bfa5eac73b1101cefa1df0ddcade2ae1
Jay3636 <58828215+Jay3636@users.noreply.github.com>
2023-03-23 14:59:28 -0400

    second

--- schema.sql
+++ schema.sql
@@ -6,3 +6,23 @@ CREATE TABLE user (
   password VARCHAR(50),
   role VARCHAR(50)
 );
+
+CREATE TABLE team (
+  teamId INT PRIMARY KEY,
+  teamName VARCHAR(50),
+  userid INT,
+  FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
+CREATE TABLE rubric (
+  rubricID INT PRIMARY KEY,
+  path VARCHAR(255)
+);
+
+CREATE TABLE teamAssignment (
+  teamID INT,
+  userID INT,
+  PRIMARY KEY (teamID, userID),
+  FOREIGN KEY (teamID) REFERENCES team(teamID),
+  FOREIGN KEY (userID) REFERENCES user(userID)
+);
