8a630fc524f42180810105d67e47f9f9c1b90172
Kain McDonald <sikeddemon@gmail.com>
2023-04-02 14:31:07 -0400

    new: added route for index.html

--- api/website/auth.py
+++ api/website/auth.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     return "Login"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    return "Sign Up"
+    email = request.form.get("email")
+    psw1 = request.form.get("psw")
+    psw2 = request.form.get("psw-repeat")
+
+    return render_template("index.html")
 
 @auth.route("/logout")
 def logout():
