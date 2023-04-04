bde2e505ea1a7e9ef57b86d7bb6461d138734caf
Kain McDonald <sikeddemon@gmail.com>
2023-04-03 22:25:38 -0400

    add: Login functionality added

--- api/website/auth.py
+++ api/website/auth.py
@@ -1,26 +1,39 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
 from . import db
 from .models import Users
 import hashlib, secrets
 
-auth = Blueprint("auth", __name__)
+auth = Blueprint('auth', __name__)
 
-@auth.route("/login", methods=['GET', 'POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    psw = request.form.get("psw")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        psw = request.form.get('psw')
 
-    return render_template("login.html")
+        user = Users.query.filter_by(email=email).first()
 
-@auth.route("/sign-up", methods=['GET', 'POST'])
+        if user:
+            if check_hash(psw, user.salt, user.password):
+                flash('Login was successful!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            flash('Password is incorrect.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template('login.html')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method=='POST':
         firstName = request.form.get('firstName')
         lastName = request.form.get('lastName')
-        email = request.form.get("email")
+        email = request.form.get('email')
         print(email)
-        psw1 = request.form.get("psw")
-        psw2 = request.form.get("psw-repeat")
+        psw1 = request.form.get('psw')
+        psw2 = request.form.get('psw-repeat')
 
         email_exists = Users.query.filter_by(email=email).first()
         
@@ -43,20 +56,21 @@ def sign_up():
                 role='s')
             db.session.add(user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
         
-    return render_template("signup.html")
+    return render_template('signup.html')
 
-@auth.route("/logout")
+@auth.route('/logout')
 def logout():
-    return "Logout"
+    logout_user()
+    return redirect(url_for('views.home'))
 
-# TBD: Check format of email to ensure it is a UTC email
 def check_email(email : str):
-    if "@mocs.utc.edu" in email:
+    if '@mocs.utc.edu' in email:
         return True
-    elif "@utc.edu" in email:
+    elif '@utc.edu' in email:
         return True
     return False
 
@@ -65,3 +79,10 @@ def salt_and_hash(psw:str):
     salted_password = salt + psw.encode('utf-8')
     hashed_password = hashlib.sha256(salted_password).hexdigest()
     return [salt, hashed_password]
+
+def check_hash(psw:str, salt:str, hashed_psw:str) -> bool:
+    salted_password = salt + psw.encode('utf-8')
+    hashed_password = hashlib.sha256(salted_password).hexdigest()
+    if hashed_password == hashed_psw:
+        return True
+    return False
