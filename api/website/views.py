eb2292d0984db68d6ec3acd09641fc4076abc81e
Kain McDonald <sikeddemon@gmail.com>
2023-04-03 21:52:43 -0400

    fix: Removed non-standard comments from view.py

--- api/website/views.py
+++ api/website/views.py
@@ -1,22 +1,21 @@
-from flask import Blueprint, render_template, request  # Import the necessary modules
+from flask import Blueprint, render_template, request
 from .models import Rubric
 from . import db
 from datetime import datetime
 
 
-views = Blueprint("views", __name__)  # Define the blueprint for this module
- # Declare `rows` as a global variable
- #       # Initialize `rows` as a list containing an empty dictionary
-@views.route("/")  # Define a route for the home page
+views = Blueprint("views", __name__)
+
+@views.route("/")
 def home():
-    return render_template("home.html")  # Render the home.html template
+    return render_template("home.html")
 
 @views.route("/rubrics", methods=['GET', 'POST'])
 def rubrics():
     if request.method == "POST":
         rubric_name = request.form['rubric_name']
         date_time = request.form['date-time']
-        # Send data to API endpoint
+        
         dt_obj = datetime.fromisoformat(date_time)
         mysql_date_str = dt_obj.strftime('%Y/%m/%d %H:%M:%S')
 
@@ -27,15 +26,15 @@ def rubrics():
             )
         db.session.add(rubric)
         db.session.commit()
-        # Check if request was successful
+        
         print(date_time)
         
     return render_template("rubrics.html")
 
-@views.route("/teams", methods=['GET', 'POST'])  # Define a route for the teams page
+@views.route("/teams", methods=['GET', 'POST'])
 def teams():
-    return render_template("teams.html")  # Render the teams.html template
+    return render_template("teams.html")
 
-@views.route("/assignments", methods=['GET', 'POST'])  # Define a route for the assignments page
+@views.route("/assignments", methods=['GET', 'POST'])
 def assignments():
-    return render_template("assignments.html")  # Render the assignments.html template
+    return render_template("assignments.html")
