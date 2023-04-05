49d9b4d42113097504fab9b3102569651f4cbd98
Kain McDonald <sikeddemon@gmail.com>
2023-04-05 02:45:35 -0400

    new: Authentication overhaul + testing roles function

--- api/website/views.py
+++ api/website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
-from .models import Rubric
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_required, current_user
+from .models import Template
 from . import db
 from datetime import datetime
 
@@ -8,33 +9,65 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', user=current_user)
 
-@views.route('/rubrics', methods=['GET', 'POST'])
-def rubrics():
+@login_required
+@views.route('/templates', methods=['GET', 'POST'])
+def templates():
+#    if current_user.role == 's':
+#        flash('Must be a member of faculty or a site admin to access this page.')
+#        return redirect(url_for('views.home'))
+
     if request.method == 'POST':
-        rubric_name = request.form['rubric_name']
+        template_name = request.form['rubric_name']
         date_time = request.form['date-time']
         
         dt_obj = datetime.fromisoformat(date_time)
         mysql_date_str = dt_obj.strftime('%Y/%m/%d %H:%M:%S')
 
-        rubric = Rubric(
-            rubricName=rubric_name,
-            rubricDate=mysql_date_str,
+        template = Template(
+            templateName=template_name,
+            templateDate=mysql_date_str,
             teacherID=1,
         )
-        db.session.add(rubric)
+        db.session.add(template)
         db.session.commit()
-        
-        print(date_time)
     
-    return render_template('rubrics.html')
+    return render_template('template.html', user=current_user)
 
 @views.route('/teams', methods=['GET', 'POST'])
 def teams():
-    return render_template('teams.html')
+    return render_template('teams.html', user=current_user)
 
 @views.route('/assignments', methods=['GET', 'POST'])
 def assignments():
-    return render_template('assignments.html')
+    return render_template('assignments.html', user=current_user)
+
+
+
+##########################
+# DELETE BEFORE DELIVERY #
+##########################
+@views.route('/make-student')
+@login_required
+def make_student():
+    current_user.role = 's'
+    db.session.commit()
+    flash('You were made a student.', category='success')
+    return redirect(url_for('views.home'))
+
+@views.route('/make-faculty')
+@login_required
+def make_faculty():
+    current_user.role = 'f'
+    db.session.commit()
+    flash('You were made faculty.', category='success')
+    return redirect(url_for('views.home'))
+
+@views.route('/make-admin')
+@login_required
+def make_admin():
+    current_user.role = 'a'
+    db.session.commit()
+    flash('You were made an admin.', category='success')
+    return redirect(url_for('views.home'))
