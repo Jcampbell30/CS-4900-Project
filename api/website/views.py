eadb3f4f2d46cba4ee5cd2d69d5cf621b9613738
Jcampbell30 <campbellj19.com>
2023-04-11 00:26:05 -0400

    added: read-only views for questions

--- api/website/views.py
+++ api/website/views.py
@@ -64,50 +64,67 @@ def templates():
         )
         db.session.add(template)
         db.session.commit()
-        
+        flash(f'The {template_name} template was created successfully.', category='success')
+        return redirect(url_for('views.faculty'))
     return render_template('template.html', user=current_user)
 
 @views.route("/questions", methods = ['GET','POST'])
 def questions():
+    
     if current_user.role == 's':
         flash("Must not be a student")
-        return redirect(url_for("views.home"))
-
-    if request.method=='POST':
-        if 'templateSelection' in request.form:
-            template_id = request.form['templateSelection']
-        else:
-            template_id = request.form['template_id']
-        template = Template.query.filter_by(templateID=template_id).first()
-        num_questions = template.numberQuestions
-        template_name = template.templateName
-        if 'templateSelection' not in request.form:
+        return redirect(url_for("views.home")) 
+    if 'templateSelection' in request.form:
+        template_id = request.form['templateSelection']
+    else:
+        template_id = request.form.get('template_id')  
+    template = Template.query.filter_by(templateID=template_id).first()
+    question = Question.query.filter_by(templateID=template_id).all()
+    print(f"question: {len(question)}")
+    if len(question) == 0:
+        created = False
+        if request.method=='POST':
+            if 'templateSelection' in request.form:
+                template_id = request.form['templateSelection']
+            else:
+                template_id = request.form['template_id']
+            template = Template.query.filter_by(templateID=template_id).first()
+            num_questions = template.numberQuestions
+            template_name = template.templateName
+            print(f"template id: {template_id}")
+            print(f"template name: {template_name}")
             print(f"number of questions: {num_questions}")
-            for i in range (num_questions):
-                print(i)
-            for i in range(num_questions):
-                question_description = request.form.get("question{}".format(i+1))
-                question = Question(questionDesc = question_description, templateID=template_id)
-                db.session.add(question)
-                db.session.commit()
         
-        return render_template("questions.html", user = current_user, num_questions = num_questions,template_name = template_name, template_id=template_id)
-    flash('Please pick a template from the list of templates.', category='error')
+            if 'templateSelection' not in request.form:
+                print(f"number of questions: {num_questions}")
+                for i in range (num_questions):
+                    print(i)
+                for i in range(num_questions):
+                    question_description = request.form.get("question{}".format(i))
+                    question = Question(questionDesc = question_description, templateID=template_id)
+                    db.session.add(question)
+                    db.session.commit()
+                if num_questions > 0:
+                    flash(f'Questions added successfully to the {template_name} template.', category='success')
+                    return redirect(url_for('views.questions', template_id=template_id))
+            
+            return render_template("questions.html", user = current_user, num_questions = num_questions,template_name = template_name, template_id=template_id)
+    else:
+        created = True
+        print("question is not none")
+        template = Template.query.filter_by(templateID=template_id).first()
+        template_name = template.templateName
+        my_questions = []
+        num_questions = len(question)
+        for i in range(len(question)):
+            my_questions.append(question[i].questionDesc)
+        print(my_questions)
+        print("template_id:  {template}")
+        return render_template("questions.html", user = current_user, template_id=template_id, my_questions=my_questions, num_questions=num_questions,created = created, template_name = template.templateName)
+        
     return redirect(url_for('views.faculty'))
 
-    created = False #boolean to check if the assignments have been saved to the template
 
-    """
-    try and return the questionsAssignmentID from the templateAssignmentID
-    if I can't return it then go to create view else I got to a table view of the questions in the template
-    
-    """
-    
-  
-"""
-    IF not created: 
-        create view. 
-"""
 @views.route('/teams', methods=['GET', 'POST'])
 @login_required
 def teams():
